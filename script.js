9df9ec1a9c5c6b5c326d4a5cfbb662eb6e8cf9a3
Lauro Pereira <lauropera01@gmail.com>
2022-06-20 00:49:09 -0300

    colocando hofs

--- script.js
+++ script.js
@@ -1,98 +1,82 @@
-const taskInput = document.getElementById("texto-tarefa");
-const btnAddTask = document.getElementById("criar-tarefa");
-const taskList = document.getElementById("lista-tarefas");
-const btnClear = document.querySelector("#apaga-tudo");
-const btnClearCompleted = document.querySelector("#remover-finalizados");
-const btnSaveTasks = document.querySelector("#salvar-tarefas");
-const btnMoveUp = document.querySelector("#mover-cima");
-const btnMoveDown = document.querySelector("#mover-baixo");
-const btnRemoveTask = document.querySelector("#remover-selecionado");
+const taskInput = document.getElementById('texto-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const selectedTask = document.querySelector('#selected');
 
 function capitalizeFirstLetter() {
   return taskInput.value.charAt(0).toUpperCase() + taskInput.value.slice(1);
 }
 
 function removeSelectedTask() {
-  const selectedTask = document.querySelector("#selected");
-  if (selectedTask != null) {
-    btnRemoveTask.addEventListener("click", () => {
+  const btnRemoveTask = document.querySelector('#remover-selecionado');
+  const selectedTask = document.querySelector('#selected');
+  if (selectedTask !== null) {
+    btnRemoveTask.addEventListener('click', () => {
       selectedTask.remove();
     });
   }
 }
 
 function selectTask(event) {
-  for (let i = 0; i < taskList.children.length; i += 1) {
-    taskList.children[i].removeAttribute("id");
-  }
-  const task = event.target;
-  task.id = 'selected';
+  taskList.childNodes.forEach((task) => task.removeAttribute('id'));
+  event.target.id = 'selected';
   removeSelectedTask();
 }
 
 function createTask() {
-  btnAddTask.addEventListener("click", () => {
+  const btnAddTask = document.getElementById('criar-tarefa');
+  btnAddTask.addEventListener('click', () => {
     if (taskInput.value.trim().length === 0) {
-      alert("Insira uma tarefa.");
+      alert('Insira uma tarefa.');
     } else {
-      const newListItem = document.createElement("li");
+      const newListItem = document.createElement('li');
       newListItem.innerText = capitalizeFirstLetter();
-      newListItem.addEventListener("click", selectTask);
-      newListItem.addEventListener("dblclick", (event) => {
-        event.target.classList.toggle("completed");
+      newListItem.addEventListener('click', selectTask);
+      newListItem.addEventListener('dblclick', (event) => {
+        event.target.classList.toggle('completed');
       });
       taskList.appendChild(newListItem);
     }
-    taskInput.value = "";
+    taskInput.value = '';
   });
 }
 
 function clearTasks() {
-  btnClear.addEventListener("click", () => {
-    while (taskList.firstChild) {
-      taskList.removeChild(taskList.firstChild);
-    }
-    // Me ajudou: https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
-    localStorage.removeItem("tasks");
+  const btnClear = document.querySelector('#apaga-tudo');
+  btnClear.addEventListener('click', () => {
+    while (taskList.firstChild) taskList.removeChild(taskList.firstChild);
+    localStorage.removeItem('tasks');
   });
 }
 
 function clearCompleted() {
-  btnClearCompleted.addEventListener("click", () => {
-    const taskCompleted = document.querySelectorAll(".completed");
-    for (let i = 0; i < taskCompleted.length; i += 1) {
-      taskCompleted[i].remove();
-    }
+  const btnClearCompleted = document.querySelector('#remover-finalizados');
+  const taskCompleted = document.querySelectorAll('.completed');
+  btnClearCompleted.addEventListener('click', () => {
+    taskCompleted.forEach((task) => task.remove())
   });
 }
 
 function saveTasks() {
-  for (let i = 0; i < taskList.children.length; i += 1) {
-    taskList.children[i].removeAttribute("id");
-  }
-  btnSaveTasks.addEventListener("click", () => {
-    localStorage.setItem("tasks", taskList.innerHTML);
+  const btnSaveTasks = document.querySelector('#salvar-tarefas');
+  taskList.childNodes.forEach((task) => task.removeAttribute('id'))
+  btnSaveTasks.addEventListener('click', () => {
+    localStorage.setItem('tasks', taskList.innerHTML);
   });
 }
 
 function loadTasks() {
-  taskList.innerHTML = localStorage.getItem("tasks");
-  for (let i = 0; i < taskList.children.length; i += 1) {
-    const task = taskList.children[i];
-    task.addEventListener("click", selectTask);
-    task.addEventListener("dblclick", (event) => {
-      event.target.classList.toggle("completed");
-    });
-  }
+  taskList.innerHTML = localStorage.getItem('tasks');
+  taskList.childNodes.forEach((task) => {
+    task.addEventListener('click', selectTask);
+    task.addEventListener('dblclick', (event) => {
+      event.target.classList.toggle('completed');
+  });
+})
 }
 
-// Nas funções moveUp e moveDown consultei os seguintes links para me ajudar:
-// https://iqcode.com/code/javascript/move-list-items-up-and-down-using-javascript
-// https://www.w3schools.com/jsref/met_node_insertbefore.asp
-// E também consultei o projeto de um colega para apenas procurar entender melhor como montar a condição: Elias Ferreira.
 function moveUp() {
-  btnMoveUp.addEventListener("click", () => {
-    const selectedTask = document.querySelector("#selected");
+  const btnMoveUp = document.querySelector('#mover-cima');
+  btnMoveUp.addEventListener('click', () => {
     if (selectedTask && selectedTask.previousElementSibling) {
       taskList.insertBefore(selectedTask, selectedTask.previousElementSibling);
     }
@@ -100,8 +84,8 @@ function moveUp() {
 }
 
 function moveDown() {
-  btnMoveDown.addEventListener("click", () => {
-    const selectedTask = document.querySelector("#selected");
+  const btnMoveDown = document.querySelector('#mover-baixo');
+  btnMoveDown.addEventListener('click', () => {
     if (selectedTask && selectedTask.nextElementSibling) {
       taskList.insertBefore(selectedTask.nextElementSibling, selectedTask);
     }
